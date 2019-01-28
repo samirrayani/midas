6eae0c05f10775bff81921cdac2668164a6efcfc
Samir Rayani <samir.rayani@gmail.com>
2019-01-28 11:21:08 -0500

    check for jquery $

--- site-assets/site.js
+++ site-assets/site.js
@@ -1,15 +1,17 @@
-$(document).ready(function(){
-	$.get("/Midas.js?"+Math.random(), undefined, function(data) {
-		$('#midasjs').text(data);
-		hljs.initHighlighting();
+if( $ ) {
+	$(document).ready(function(){
+		$.get("/Midas.js?"+Math.random(), undefined, function(data) {
+			$('#midasjs').text(data);
+			hljs.initHighlighting();
 
-		$('#midasjs').each(function(i, block) {
-			hljs.lineNumbersBlock(block);
+			$('#midasjs').each(function(i, block) {
+				hljs.lineNumbersBlock(block);
+			});
+		});
+
+		$.get("/LICENSE.txt?"+Math.random(), undefined, function(data) {
+			$('#license').text(data);
+			hljs.initHighlighting();
 		});
 	});
-
-	$.get("/LICENSE.txt?"+Math.random(), undefined, function(data) {
-		$('#license').text(data);
-		hljs.initHighlighting();
-	});
-});
+}
