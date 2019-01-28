d2b88997094bdf9aa8e808bb0c25b8ac6534ba4e
Samir Rayani <samir.rayani@gmail.com>
2019-01-28 14:12:13 -0500

    restructure directory

--- site/assets/site.js
+++ site/assets/site.js
@@ -1,7 +1,5 @@
 if( $ ) {
-	$(document).ready(function(){
-		new ClipboardJS('.btn');
-		
+	$(document).ready(function(){		
 		$.get("/Midas.js?"+Math.random(), undefined, function(data) {
 			$('#midasjs').text(data);
 			hljs.initHighlighting();
